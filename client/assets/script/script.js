c56f31937e83c0a20003eeb42ddd92c8b9e368b0
Nandira Paturohman <nandirasp@gmail.com>
2017-11-23 22:16:08 +0700

    add modal create new comic

--- client/assets/script/script.js
+++ client/assets/script/script.js
@@ -19,3 +19,18 @@ $(document)
     ;
   })
 ;
+
+$("input:text").click(function() {
+  $(this).parent().find("input:file").click();
+});
+
+$('input:file', '.ui.action.input')
+.on('change', function(e) {
+  console.log(e.target.files);
+  if (e.target.files.length > 1) {
+    var name = `${e.target.files.length} file dipilih.`
+  }else{
+    var name = e.target.files[0].name;
+  }
+  $('input:text', $(e.target).parent()).val(name);
+});
--- client/assets/script/vue/vue-parent.js
+++ client/assets/script/vue/vue-parent.js
@@ -3,6 +3,24 @@ new Vue({
   data: {
     message: 'Hello Vue!',
     login: '',
-    loop: [1,1,1,1,1,1,1,1,1]
+    loop: [1,1,1,1,1,1,1,1,1,1,1,1,1,],
+    comicList: []
+  },
+  methods: {
+     modalAdd: function() {
+       console.log('masuk');
+       $('.tiny.modal.modal-add')
+        .modal('show')
+      ;
+     }
+  },
+  created: function () {
+    axios.get('https://localhost:3000/comic')
+    .then(response => {
+      this.comicList = response
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 })
